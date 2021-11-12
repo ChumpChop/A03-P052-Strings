fdcf8430ca72fa215f4726c87067241250c52dd2
Clive B Harrison <clive_b_harrison@outlook.com>
2021-11-12 22:17:10 +0000

    Add project files.

---
+++ Strings CBH/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnStrings = New System.Windows.Forms.Button()
+        Me.btnConcatenation = New System.Windows.Forms.Button()
+        Me.txtString = New System.Windows.Forms.TextBox()
+        Me.btnLength = New System.Windows.Forms.Button()
+        Me.btnSubStrings = New System.Windows.Forms.Button()
+        Me.btnReplace = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnStrings
+        '
+        Me.btnStrings.Location = New System.Drawing.Point(12, 12)
+        Me.btnStrings.Name = "btnStrings"
+        Me.btnStrings.Size = New System.Drawing.Size(181, 23)
+        Me.btnStrings.TabIndex = 0
+        Me.btnStrings.Text = "Using Strings"
+        Me.btnStrings.UseVisualStyleBackColor = True
+        '
+        'btnConcatenation
+        '
+        Me.btnConcatenation.Location = New System.Drawing.Point(12, 41)
+        Me.btnConcatenation.Name = "btnConcatenation"
+        Me.btnConcatenation.Size = New System.Drawing.Size(181, 23)
+        Me.btnConcatenation.TabIndex = 1
+        Me.btnConcatenation.Text = "Concatenation"
+        Me.btnConcatenation.UseVisualStyleBackColor = True
+        '
+        'txtString
+        '
+        Me.txtString.Location = New System.Drawing.Point(12, 70)
+        Me.txtString.Name = "txtString"
+        Me.txtString.Size = New System.Drawing.Size(100, 20)
+        Me.txtString.TabIndex = 2
+        '
+        'btnLength
+        '
+        Me.btnLength.Location = New System.Drawing.Point(118, 70)
+        Me.btnLength.Name = "btnLength"
+        Me.btnLength.Size = New System.Drawing.Size(75, 23)
+        Me.btnLength.TabIndex = 3
+        Me.btnLength.Text = "Length"
+        Me.btnLength.UseVisualStyleBackColor = True
+        '
+        'btnSubStrings
+        '
+        Me.btnSubStrings.Location = New System.Drawing.Point(12, 96)
+        Me.btnSubStrings.Name = "btnSubStrings"
+        Me.btnSubStrings.Size = New System.Drawing.Size(75, 23)
+        Me.btnSubStrings.TabIndex = 4
+        Me.btnSubStrings.Text = "SubStrings"
+        Me.btnSubStrings.UseVisualStyleBackColor = True
+        '
+        'btnReplace
+        '
+        Me.btnReplace.Location = New System.Drawing.Point(118, 99)
+        Me.btnReplace.Name = "btnReplace"
+        Me.btnReplace.Size = New System.Drawing.Size(75, 23)
+        Me.btnReplace.TabIndex = 5
+        Me.btnReplace.Text = "Replace"
+        Me.btnReplace.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(292, 181)
+        Me.Controls.Add(Me.btnReplace)
+        Me.Controls.Add(Me.btnSubStrings)
+        Me.Controls.Add(Me.btnLength)
+        Me.Controls.Add(Me.txtString)
+        Me.Controls.Add(Me.btnConcatenation)
+        Me.Controls.Add(Me.btnStrings)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnStrings As Button
+    Friend WithEvents btnConcatenation As Button
+    Friend WithEvents txtString As TextBox
+    Friend WithEvents btnLength As Button
+    Friend WithEvents btnSubStrings As Button
+    Friend WithEvents btnReplace As Button
+End Class
---
+++ Strings/Form1.Designer.vb
@@ -0,0 +1,110 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnStrings = New System.Windows.Forms.Button()
+        Me.btnConcatenation = New System.Windows.Forms.Button()
+        Me.txtString = New System.Windows.Forms.TextBox()
+        Me.btnLength = New System.Windows.Forms.Button()
+        Me.btnSubStrings = New System.Windows.Forms.Button()
+        Me.btnReplace = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnStrings
+        '
+        Me.btnStrings.Location = New System.Drawing.Point(13, 13)
+        Me.btnStrings.Name = "btnStrings"
+        Me.btnStrings.Size = New System.Drawing.Size(182, 23)
+        Me.btnStrings.TabIndex = 0
+        Me.btnStrings.Text = "Using Strings"
+        Me.btnStrings.UseVisualStyleBackColor = True
+        '
+        'btnConcatenation
+        '
+        Me.btnConcatenation.Location = New System.Drawing.Point(13, 43)
+        Me.btnConcatenation.Name = "btnConcatenation"
+        Me.btnConcatenation.Size = New System.Drawing.Size(182, 23)
+        Me.btnConcatenation.TabIndex = 1
+        Me.btnConcatenation.Text = "Concatenation"
+        Me.btnConcatenation.UseVisualStyleBackColor = True
+        '
+        'txtString
+        '
+        Me.txtString.Location = New System.Drawing.Point(13, 73)
+        Me.txtString.Name = "txtString"
+        Me.txtString.Size = New System.Drawing.Size(100, 20)
+        Me.txtString.TabIndex = 2
+        '
+        'btnLength
+        '
+        Me.btnLength.Location = New System.Drawing.Point(120, 72)
+        Me.btnLength.Name = "btnLength"
+        Me.btnLength.Size = New System.Drawing.Size(75, 23)
+        Me.btnLength.TabIndex = 3
+        Me.btnLength.Text = "Length"
+        Me.btnLength.UseVisualStyleBackColor = True
+        '
+        'btnSubStrings
+        '
+        Me.btnSubStrings.Location = New System.Drawing.Point(12, 101)
+        Me.btnSubStrings.Name = "btnSubStrings"
+        Me.btnSubStrings.Size = New System.Drawing.Size(181, 23)
+        Me.btnSubStrings.TabIndex = 4
+        Me.btnSubStrings.Text = "SubStrings"
+        Me.btnSubStrings.UseVisualStyleBackColor = True
+        '
+        'btnReplace
+        '
+        Me.btnReplace.Location = New System.Drawing.Point(13, 131)
+        Me.btnReplace.Name = "btnReplace"
+        Me.btnReplace.Size = New System.Drawing.Size(180, 23)
+        Me.btnReplace.TabIndex = 5
+        Me.btnReplace.Text = "Replace"
+        Me.btnReplace.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(206, 163)
+        Me.Controls.Add(Me.btnReplace)
+        Me.Controls.Add(Me.btnSubStrings)
+        Me.Controls.Add(Me.btnLength)
+        Me.Controls.Add(Me.txtString)
+        Me.Controls.Add(Me.btnConcatenation)
+        Me.Controls.Add(Me.btnStrings)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnStrings As System.Windows.Forms.Button
+    Friend WithEvents btnConcatenation As System.Windows.Forms.Button
+    Friend WithEvents txtString As System.Windows.Forms.TextBox
+    Friend WithEvents btnLength As System.Windows.Forms.Button
+    Friend WithEvents btnSubStrings As System.Windows.Forms.Button
+    Friend WithEvents btnReplace As System.Windows.Forms.Button
+
+
+End Class
